053554b58030d70743884e8ecbcc824657a00596
Fabien Arrault <farrault@ippon.fr>
2018-04-21 17:22:53 +0200

    Define the UserInfoTokenServices in a dedicaded @Configuration class and comment spring boot security.oauth2.resource property that are not used in the end because of the presence of our custom UserInfoTokenServices (see https://github.com/jhipster/generator-jhipster/issues/7281)

--- ms/src/main/java/com/mycompany/oauth2ms/config/SecurityConfiguration.java
+++ ms/src/main/java/com/mycompany/oauth2ms/config/SecurityConfiguration.java
@@ -20,39 +20,11 @@ import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
-    private static final String OAUTH2_PRINCIPAL_ATTRIBUTE = "preferred_username";
-
-    private static final String OAUTH2_AUTHORITIES_ATTRIBUTE = "roles";
-
-    private final ResourceServerProperties resourceServerProperties;
-
     private final SecurityProblemSupport problemSupport;
 
     public SecurityConfiguration(ResourceServerProperties resourceServerProperties,
         SecurityProblemSupport problemSupport) {
-        this.resourceServerProperties = resourceServerProperties;
         this.problemSupport = problemSupport;
-    }
-
-    @Bean
-    @Primary
-    public UserInfoTokenServices userInfoTokenServices(PrincipalExtractor principalExtractor, AuthoritiesExtractor authoritiesExtractor) {
-        UserInfoTokenServices userInfoTokenServices =
-            new CachedUserInfoTokenServices(resourceServerProperties.getUserInfoUri(), resourceServerProperties.getClientId());
-
-        userInfoTokenServices.setPrincipalExtractor(principalExtractor);
-        userInfoTokenServices.setAuthoritiesExtractor(authoritiesExtractor);
-        return userInfoTokenServices;
-    }
-
-    @Bean
-    public PrincipalExtractor principalExtractor() {
-        return new SimplePrincipalExtractor(OAUTH2_PRINCIPAL_ATTRIBUTE);
-    }
-
-    @Bean
-    public AuthoritiesExtractor authoritiesExtractor() {
-        return new SimpleAuthoritiesExtractor(OAUTH2_AUTHORITIES_ATTRIBUTE);
     }
 
     @Bean
